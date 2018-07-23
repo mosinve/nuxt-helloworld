14507bf0958253606b3ea1d0fff375f42afdfe1d
Vitaly Mosin <mosinve@gmail.com>
2018-07-23 11:46:22 +0700

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id='app'>
+    <nuxt/>
+  </div>
+</template>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #222fb9;
+  }
+</style>
+
