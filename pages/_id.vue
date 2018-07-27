e2bfc73752d3c3971160a76193078661e69ad6cf
Vitaly Mosin <mosinve@gmail.com>
2018-07-28 02:19:32 +0700

    redesign app

--- pages/_id.vue
+++ pages/_id.vue
@@ -1,17 +1,23 @@
-<template>
-  <div>
-    <article v-if='item'>
-      <h1>{{ item.attributes.canonicalTitle }}</h1>
-      <table>
-        <tbody>
-        <tr>
-          <td><img :src='item.attributes.posterImage.medium'></td>
-          <td>{{ item.attributes.synopsis }}</td>
-        </tr>
-        </tbody>
-      </table>
-    </article>
-  </div>
+<template lang='pug'>
+  .film-details
+    div(v-if='item')
+      nav(aria-label="breadcrumb")
+        ol.breadcrumb
+          li.breadcrumb-item
+            router-link(:to="{ name: 'index' }") Home
+          li.breadcrumb-item.active(aria-current="page") {{item.attributes.canonicalTitle}}
+      article.card
+        .card-header {{ item.attributes.canonicalTitle }}
+        table.table
+          tbody
+            tr
+              td: img( :src='item.attributes.posterImage.medium')
+              td {{ item.attributes.synopsis }}
+            tr
+              td(colspan=2 align='center')
+                iframe(width="854" height="480" :src="`https://www.youtube.com/embed/${item.attributes.youtubeVideoId}?ecver=1`" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen>)
+    .text-center(v-else)
+      font-awesome-icon( icon="spinner" size="6x" spin)
 </template>
 
 <script>
